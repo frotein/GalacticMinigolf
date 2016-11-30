08996795ae9efbe3fd1551042be352209ab2feb4
dean thurston <deanthurston2791@gmail.com>
2016-11-29 20:15:11 -0600

    added prediction model
    works for balls hit in air, not from planet yet

--- Assets/ClickAndDragForce.cs
+++ Assets/ClickAndDragForce.cs
@@ -7,17 +7,26 @@ public class ClickAndDragForce : MonoBehaviour {
     Collider2D collider;
     bool grabbing;
     Rigidbody2D rigidBody;
+    public OrbitPredictor preditor;
+    public Vector2 initialForce;
+    bool nextFrame = false;
     // Use this for initialization
 	void Start ()
     {
         collider = transform.GetComponent<Collider2D>();
         rigidBody = transform.GetComponent<Rigidbody2D>();
+       
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-	    if(Input.GetMouseButtonDown(0) && !grabbing)
+        if(nextFrame)
+        {
+            nextFrame = false;
+        }
+       
+        if (Input.GetMouseButtonDown(0) && !grabbing)
         {
             Vector2 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             if (collider.OverlapPoint(worldPos))
@@ -26,13 +35,35 @@ public class ClickAndDragForce : MonoBehaviour {
             }
         }
 
+        if(grabbing)
+        {
+            Vector2 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 dirAndPower = worldPos - (Vector2)transform.position;
+            preditor.Simulate(rigidBody, dirAndPower * -forceIncreaseRate, 100);
+        }
         if(Input.GetMouseButtonUp(0) && grabbing)
         {
             ApplyForce();
             grabbing = false;
         }
+
+        if(Input.GetKeyDown("m"))
+        {
+            rigidBody.isKinematic = false;
+            rigidBody.AddForce(initialForce);
+            nextFrame = true;
+        }
+
+        
 	}
 
+    void FixedUpdate()
+    {
+        if (!rigidBody.isKinematic)
+        {
+            preditor.actualVels.Add(rigidBody.velocity);
+        }
+    }
     void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.layer == LayerMask.NameToLayer("Planet"))
---
+++ Assets/OrbitPredictor.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class OrbitPredictor : MonoBehaviour {
+
+    float mass;
+    float drag;
+    Vector2 position;
+    Vector2 velocity;
+    List<Vector3> allPositions;
+    public List<Vector2> velocities;
+    public List<Vector2> actualVels;
+    public List<PointEffector2D> effectors;
+    public LineRenderer lineRenderer;
+    // Use this for initialization
+	void Start ()
+    {
+        actualVels = new List<Vector2>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void Simulate(Rigidbody2D body, Vector2 initialForce, int steps)
+    {
+        Vector2 vel = (initialForce / body.mass) * Time.fixedDeltaTime;
+        Debug.Log("predicted" + vel.y);
+        drag = body.drag;
+        InitialValues(body.position, vel, body.mass);
+        allPositions = new List<Vector3>();
+        velocities = new List<Vector2>();
+        velocities.Add(velocity);
+        allPositions.Add(body.position);
+        for (int i = 0; i < steps; i++)
+        {
+            PhysicsStep();
+            allPositions.Add(new Vector3(position.x, position.y,0));
+            velocities.Add(velocity);
+        }
+
+        Render();
+    }
+
+    void Render()
+    {
+        lineRenderer.SetVertexCount(allPositions.Count);
+        lineRenderer.SetPositions(allPositions.ToArray());
+    }
+    void InitialValues(Vector2 pos, Vector2 vel, float mas)
+    {
+        mass = mas;
+        position = pos;
+        velocity = vel;
+    }
+    void PhysicsStep()
+    {
+        foreach (PointEffector2D effector in effectors)
+        {
+            Vector2 dir = new Vector2(effector.transform.position.x - position.x, effector.transform.position.y - position.y);
+            float distSqr = dir.magnitude * effector.distanceScale;
+            if (effector.forceMode == EffectorForceMode2D.InverseSquared)
+                distSqr *= distSqr;
+            float force =  effector.forceMagnitude / (distSqr);
+            float vel = (force / mass);
+            vel*= Time.fixedDeltaTime;
+            
+            velocity += -dir.normalized * vel;
+           
+        }
+        velocity *= (1f - (Time.fixedDeltaTime * drag));
+        position += velocity * Time.fixedDeltaTime;
+    }
+}
